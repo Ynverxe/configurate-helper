b7704cdaf3372819ecbcd314b207e641c3d184da
Ynverxe <nahubar65@gmail.com>
2025-08-02 12:28:14 -0300

    build: set target and source version to 8, remove usage of languageVersion

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,9 +18,8 @@ java {
     withSourcesJar()
     withJavadocJar()
 
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(8)
-    }
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
 }
 
 dependencies {
