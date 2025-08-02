725272e0c79ab5b417dac44b8ed58171efdd2769
Ynverxe <nahubar65@gmail.com>
2025-08-02 13:17:08 -0300

    Revert "build: set target and source version to 8, remove usage of languageVersion"
    This reverts commit b7704cdaf3372819ecbcd314b207e641c3d184da.

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,8 +18,9 @@ java {
     withSourcesJar()
     withJavadocJar()
 
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(8)
+    }
 }
 
 dependencies {
